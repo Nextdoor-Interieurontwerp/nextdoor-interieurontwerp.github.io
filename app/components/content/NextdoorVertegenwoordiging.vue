da04a9e5226901156b9de7ddb43fc3364b02f6c1
Rick Pennings <rick@intractief.nl>
2026-03-16 13:20:07 +0100

    Fixed header size for Vertegenwoordiging for mobile

--- app/components/content/NextdoorVertegenwoordiging.vue
+++ app/components/content/NextdoorVertegenwoordiging.vue
@@ -12,7 +12,7 @@
           <p class="member-name">Petra Willems</p>
           <p class="member-desc">{{ $t('vertegenwoordiging.memberDesc') }}</p>
           <a href="mailto:petra@nextdoorinterieurontwerp.nl" class="btn-orange">
-            <svg class="btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/></svg>
+            <svg class="btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/></svg>
             {{ $t('vertegenwoordiging.emailBtn') }}
           </a>
         </div>
@@ -83,6 +83,10 @@
   }
   .member-name {
     font-size: 3.5rem;
+  }
+
+  .section-title {
+    font-size: 2.5rem;
   }
 }
 </style>
